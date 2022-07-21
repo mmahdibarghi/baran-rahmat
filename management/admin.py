d5a9ca7fff0f0f4de6579ae59d1f48206ed4dee5
yasinBakhtiari <bakhtiariyasin80@gmail.com>
2022-07-21 18:55:35 +0430

    customizing page class

--- management/admin.py
+++ management/admin.py
@@ -1,9 +1,47 @@
 from django.contrib import admin
 from . import models
 # Register your models here.
-admin.site.register(models.Payment)
-admin.site.register(models.Khayer)
-admin.site.register(models.HesabMoaseseh)
-admin.site.register(models.Madadjo)
-admin.site.register(models.SandoghKhayerieh)
-admin.site.register(models.TahvilgirandehSandogh)
+
+'''@admin.register(models.Payment)
+class AdminPayment(admin.ModelAdmin):
+    list_display=['pk']
+    list_editable=['pk']
+    #ordering=['']
+    list_per_page=15
+'''
+@admin.register(models.Khayer)
+class AdminKhayer(admin.ModelAdmin):
+    list_display=['pk','first_name']
+    list_editable=['first_name']
+    #ordering=['']
+    list_per_page=15
+'''
+@admin.register(models.HesabMoaseseh)
+class AdminHesabMoaseseh(admin.ModelAdmin):
+    list_display=['']
+    list_editable=['']
+    #ordering=['']
+    list_per_page=15
+
+@admin.register(models.Madadjo)
+class AdminMadadjo(admin.ModelAdmin):
+    list_display=['']
+    list_editable=['']
+    #ordering=['']
+    list_per_page=15
+
+@admin.register(models.TahvilgirandehSandogh)
+class AdminTahvilgirandehSandogh(admin.ModelAdmin):
+    list_display=['']
+    list_editable=['']
+    #ordering=['']
+    list_per_page=15
+
+@admin.register(models.SandoghKhayerieh)
+class AdminSandoghKhayerieh(admin.ModelAdmin):
+    list_display=['']
+    list_editable=['']
+    #ordering=['']
+    list_per_page=15
+
+'''
